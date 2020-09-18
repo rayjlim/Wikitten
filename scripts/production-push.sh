aeef722a64674e9047a00eb7135f66e53e1e27ee
ray <rayjlim1@gmail.com>
2020-09-17 22:36:20 -0700

    feat:🔥 remove SPA from build

--- scripts/production-push.sh
+++ scripts/production-push.sh
@@ -10,13 +10,11 @@ fi
 echo "FTP HOST " $FTP_HOST
 
 mkdir -p $PREP_DIR
-mkdir -p $PREP_DIR-ui
 
 while getopts rsn option; do
   case "${option}" in
     r) RESETSSH=true;;
     s) BUILD=true;;
-    n) NOBUILDSPA=true;;
   esac
 done
 
@@ -24,19 +22,6 @@ if [ -z $BUILD ]; then
   rsync -ravz --exclude-from 'scripts/production-exclude.txt' --delete . $PREP_DIR
   rsync -avz  scripts/production-exclude-push.txt $PREP_DIR/production-exclude-push.txt
   rsync -avz  config.php.production $PREP_DIR/config.php
-
-  if [ -z "$NOBUILDSPA" ]; then
-    cd ui-notesee
-    npm run build
-    buildresult=$?
-    if [ $buildresult != 0 ]; then
-      echo "SPA Build Fail"
-      exit 1
-    fi
-    
-    rsync -ravz --delete build/* ../$PREP_DIR-ui/
-    cd ..
-  fi
 
 else
   echo "Skip Build"
@@ -61,11 +46,4 @@ rsync -rave  'ssh -oHostKeyAlgorithms=+ssh-dss' \
   --exclude-from 'production-exclude-push.txt' \
   --delete . $FTP_USER@$FTP_HOST:$FOLDER/
 
-cd ./$PREP_DIR-ui
-pwd
-echo $FTP_TARGETFOLDER
-rsync -rave  'ssh -oHostKeyAlgorithms=+ssh-dss' \
-  --delete . $FTP_USER@$FTP_HOST:$FTP_TARGETFOLDER/
-
-ssh  $FTP_USER@$FTP_HOST "chmod -R 755 $FTP_TARGETFOLDER/"
 ssh  $FTP_USER@$FTP_HOST "chmod -R 755 $FTP_TARGETFOLDER-api/"
